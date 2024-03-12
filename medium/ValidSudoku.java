39ac11585ddfd50446edbaf273ec1af29160b361
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-03-13 01:39:06 +0700

    ValidSudoku

---
+++ medium/ValidSudoku.java
@@ -0,0 +1,40 @@
+package medium;
+
+/*
+ Runtime - beats 7.12%
+ Memory - beats 8.67%
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+	public static void main(String[] args) {
+		char[][] grid = {
+				{ '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+				{ '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+				{ '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+				{ '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+				{ '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+				{ '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+				{ '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+				{ '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+				{ '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+		};
+		System.out.println(isValidSudoku(grid));
+	}
+
+	public static boolean isValidSudoku(char[][] board) {
+		Set<String> setElems = new HashSet<>();
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[i][j] != '.') {
+					String s = "(" + board[i][j] + ")";
+					if (!setElems.add(s + i) || !setElems.add(j + s) || !setElems.add(i / 3 + s + j / 3))
+						return false;
+				}
+			}
+		}
+		return true;
+	}
+}
