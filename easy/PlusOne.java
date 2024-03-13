ece35f5c5b09b89133086dec07fcf5801c635343
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-03-13 11:26:53 +0700

    Plus One

---
+++ easy/PlusOne.java
@@ -0,0 +1,26 @@
+/*
+ Runtime - beats 100%
+ Memory - beats 82%
+ */
+
+import java.util.Arrays;
+
+public class PlusOne {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(plusOne(new int[] { 9, 9 })));
+
+	}
+
+	public static int[] plusOne(int[] digits) {
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
+		}
+		digits = new int[digits.length + 1];
+		digits[0] = 1;
+		return digits;
+	}
+}
