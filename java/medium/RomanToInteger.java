a7de60eb86053a1c18bacd9fb214bf2184b40d84
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-04-15 15:28:30 +0700

    12

---
+++ java/medium/RomanToInteger.java
@@ -0,0 +1,24 @@
+package medium;
+
+// task 12
+
+public class RomanToInteger {
+	private static final int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+	private static final String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+	public static void main(String[] args) {
+		System.out.println(intToRoman(157));
+	}
+
+	public static String intToRoman(int num) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < values.length; i++) {
+			while (num >= values[i]) {
+				sb.append(symbols[i]);
+				num -= values[i];
+			}
+		}
+		return sb.toString();
+	}
+}
