257be0cdb8be6ef426d496a8e2541c6339b8ed06
Alexis Vorobyev <163092083+HildaburJavaDev@users.noreply.github.com>
2024-08-27 13:54:40 +0700

    Create CreateHelloWorldFunction.js

---
+++ JavaScript/CreateHelloWorldFunction.js
@@ -0,0 +1,16 @@
+// task №2667
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
