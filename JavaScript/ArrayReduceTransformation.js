5aea3ca503cc8e1a72fe241da74de13c335df362
Alexis Vorobyev <163092083+HildaburJavaDev@users.noreply.github.com>
2024-08-27 15:04:25 +0700

    Create ArrayReduceTransformation.js

---
+++ JavaScript/ArrayReduceTransformation.js
@@ -0,0 +1,13 @@
+// task №2626
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+    for (let i = 0; i < nums.length; i++) val = fn(val, nums[i]);
+    return val
+};
