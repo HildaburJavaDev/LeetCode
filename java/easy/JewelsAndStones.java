0c6db8bf07659441d1db2eb735389017b52ec842
Alexis Vorobyev <163092083+HildaburJavaDev@users.noreply.github.com>
2024-08-28 16:57:28 +0700

    Create JewelsAndStones.java

---
+++ java/easy/JewelsAndStones.java
@@ -0,0 +1,11 @@
+// task №771
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char c : stones.toCharArray()) {
+            if (jewels.indexOf(c) != -1) count++;
+        }
+        return count;
+    }
+}
