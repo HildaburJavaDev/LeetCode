5ac187bbff58e0460ffb12298a89f282f7f17acc
Alexis Vorobyev <163092083+HildaburJavaDev@users.noreply.github.com>
2024-08-28 15:16:53 +0700

    Create DivisibleAndNonDivisibleDifference.java

---
+++ java/easy/DivisibleAndNonDivisibleDifference.java
@@ -0,0 +1,12 @@
+// task №2894
+
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int divisible = 0, notDivisible = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i < m || i % m != 0) notDivisible += i;
+            else divisible += i;
+        }
+        return notDivisible - divisible;
+    }
+}
