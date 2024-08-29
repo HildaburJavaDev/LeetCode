654d1901ff48d4f6077264e83fb33dd5cf2f4922
Alexis Vorobyev <163092083+HildaburJavaDev@users.noreply.github.com>
2024-08-29 14:55:33 +0700

    Create CalculatorWithMethodChaining.js

---
+++ JavaScript/easy/CalculatorWithMethodChaining.js
@@ -0,0 +1,33 @@
+// task №2726
+
+class Calculator {
+    constructor(value) {
+        this.val = value;
+    }
+    add(value){
+        this.val += value;
+        return this
+    }
+    subtract(value){
+        this.val -= value;
+        return this
+    } 
+    multiply(value) {
+        this.val *= value;
+        return this
+    }
+    divide(value) {
+        if (value === 0) {
+            throw new Error('Division by zero is not allowed');
+        }
+        this.val /= value;
+        return this
+    }
+    power(value) {
+        this.val = Math.pow(this.val, value);
+        return this
+    }
+    getResult() {
+        return this.val;
+    }
+}
